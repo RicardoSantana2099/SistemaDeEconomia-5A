4a4e13241b2f845d8bdb2a10cc47222d220a751b
Ricardo Santana <pacheco2099.working@gmail.com>
2023-02-27 13:45:49 -0600

    Inventario
    Se agrego un inventario para poder hacer la compra.

---
+++ 5A-SistemaDe Economia/Assets/SistemaDeEconomia/Scripts/PlantillaObjeto.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Objeto", menuName = "ObjetoTienda")]
+public class PlantillaObjeto : ScriptableObject
+{
+    public Sprite imagenObjeto;
+    public string textoObjeto;
+    public int precioObjeto; 
+
+}
