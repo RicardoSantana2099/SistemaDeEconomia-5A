02ed8a7e97c5156a934cabb9b63bcfdfb1681ef0
Ricardo Santana <pacheco2099.working@gmail.com>
2023-03-12 14:48:48 -0600

    button
    los botones ya funcionan para cambiar de escena

---
+++ 5A-SistemaDe Economia/Assets/SistemaDeEconomia/Scripts/2.0/GameDataManager.cs
@@ -0,0 +1,46 @@
+[System.Serializable] public class PlayerData
+{
+    public int coins = 0;
+}
+public static class GameDataManager
+{
+    static PlayerData playerData = new PlayerData();
+
+    static GameDataManager()
+        {
+        LoadPlayerData();
+        }
+
+
+    public static int GetCoins()
+    {
+        return playerData.coins;
+    }
+
+    public static void AddCoins(int amount)
+    {
+        playerData.coins += amount;
+    }
+
+    public static bool CanSpendCoins (int amount)
+    {
+        return (playerData.coins >= amount);
+    }
+
+    public static void SpendCoins(int amount)
+    {
+        playerData.coins -= amount; 
+    }
+
+    static void LoadPlayerData()
+    {
+        playerData = BinarySerializer.Load<PlayerData>("player-data.txt");
+        UnityEngine.Debug.Log("<color=green>[PlayerData]Loaded.</color>");
+    }
+
+    static void SavePlayerData()
+    {
+        BinarySerializer.Save(playerData, "player-data.txt");
+        UnityEngine.Debug.Log("<color=green>[PlayerData]Saved.</color>");
+    }
+}
