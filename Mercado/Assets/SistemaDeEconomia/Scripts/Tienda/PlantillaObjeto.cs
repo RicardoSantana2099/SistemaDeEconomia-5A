e6e42366b28bb90292e4d02c30760bfb44df794d
Ricardo Santana <pacheco2099.working@gmail.com>
2023-03-13 19:05:09 -0600

    panel

---
+++ Mercado/Assets/SistemaDeEconomia/Scripts/Tienda/PlantillaObjeto.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu (fileName = "Objeto", menuName = "Objeto Tienda")]
+public class PlantillaObjeto : ScriptableObject
+{
+    public Sprite imagenObjeto;
+    public string textoObjeto;
+    public int precioObjeto;
+}
